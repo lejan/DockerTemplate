0d6e918142addab5f2cb992c1a68c44980b11a17
lejan <alem.leandro@gmail.com>
2021-10-14 08:57:26 -0300

    Update Readme modify folder structure

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-DOCKER_PATH="docker_gazebo"
 TAG_DOCKER_IMAGE="bionic_melodic_gaz9_turtlebot3"
 TAG_DOCKER_IMAGE_NVIDIA="bionic_melodic_gaz9_turtlebot3_nvidia"
-
-cd $DOCKER_PATH
 
 docker build --build-arg uid=$(id -u) --tag $TAG_DOCKER_IMAGE . -f "$(pwd)/Dockerfile"
 docker build -t $TAG_DOCKER_IMAGE_NVIDIA . -f "$(pwd)/Dockerfile.nvidia"
